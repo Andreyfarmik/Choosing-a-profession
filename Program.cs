c0c8a0adf20d7f983ad609b684ac168724680d0c
Andreyfarmik <gemefarmik@yandex.ru>
2022-10-16 11:22:25 +0500

    Написал код превращения текста в массив

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
 ﻿Console.Write("Введите данные ввиде слов, набора символов, набора цыфр, через пробел: ");
+string text = Console.ReadLine();
+string[] array = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+string[] array2 = new string[array.Length];
