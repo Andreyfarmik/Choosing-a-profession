3e2b7989e391a61971577b9d44233473e05c9991
Andreyfarmik <gemefarmik@yandex.ru>
2022-10-16 11:25:10 +0500

    Сделал метод определения строк с тримя или менее символов

--- Program.cs
+++ Program.cs
@@ -2,3 +2,15 @@
 string text = Console.ReadLine();
 string[] array = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 string[] array2 = new string[array.Length];
+void CheckArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+}
